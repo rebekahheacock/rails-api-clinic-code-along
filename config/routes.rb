ecc301e93c61ac86d3f98da33ae0f8d624a3c162
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-22 10:57:56 -0400

    Create patients routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  # get '/patients', to: 'patients#index'
+  # get '/patients/:id' to: 'patients#show'
+  # post 'patients' to: 'patients#create'
+  # patch '/patients/:id' to: 'patients#update'
+  # delete '/patients/:id' to: 'patients#destroy'
+  # in this case, to: is the same as =>
+
+  resources :patients, only: [:index, :show, :create, :update, :destroy]
 end
