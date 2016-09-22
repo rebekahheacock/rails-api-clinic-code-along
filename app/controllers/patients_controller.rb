cb0d8636485c48f6f3ca6a27ba93f9d74851180e
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-22 10:58:18 -0400

    Create Patients Controller

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,49 @@
+class PatientsController < ApplicationController
+  # calls set_patient bethod before the show, update, and destroy methods
+  before_action :set_patient, only: [:show, :update, :destroy]
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  # requires parameter data (from CRUD/AJAX) to be
+  # formatted with a root key of "patient"
+  # and allows the parameters name, sickness, and age
+  def patient_params
+    params.require(:patient).permit(:name, :sickness, :age)
+  end
+
+  def index
+    @patients = Patient.all
+    render json: @patients
+  end
+
+  def show
+    render json: Patient.find(params[:id])
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      render json: @patient, status: :created
+    else
+      render json: @patient.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    # patient is being located by set_patient
+    # attempting to update using params that were passed in
+    if @patient.update(patient_params)
+      head :no_content
+    else
+      render json: @patient.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @patient.destroy
+    head :no_content
+  end
+end
