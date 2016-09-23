509c67bce63fa71dcebf1eaaf3792ffc586a0e7b
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-23 15:27:33 -0400

    Create doctors table

---
+++ scripts/doctors.sh
@@ -0,0 +1,23 @@
+curl --include --request POST http://localhost:3000/doctors \
+  --header "Content-Type: application/json" \
+  --data '{
+    "doctor": {
+      "given_name": "Jane",
+      "surname": "Franklin",
+    }
+  }'
+
+curl --include --request GET http://localhost:3000/doctors
+
+curl --include --request GET http://localhost:3000/doctors/5
+
+curl --include --request PATCH http://localhost:3000/doctors/5 \
+  --header "Content-Type: application/json" \
+  --data '{
+    "doctor": {
+      "given_name": "Jane",
+      "surname": "Franklin",
+    }
+  }'
+
+curl --include --request DELETE http://localhost:3000/doctors/5
